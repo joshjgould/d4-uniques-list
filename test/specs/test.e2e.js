84c515c391a0955a5f7432e2a57d96b99cc34398
Josh Gould <josh@gameenforcer.com>
2024-11-03 18:50:20 -0500

    get list of build variants

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,8 +2,8 @@ const { expect, browser, $ } = require('@wdio/globals');
 
 const buildList = [];
 
-describe('Get a list of builds', () => {
-    it('Go to maxroll tierlist', async () => {
+describe('Get a list of uniques used in builds', () => {
+    it('Get a list of builds from maxroll tier list', async () => {
         await browser.url(`https://maxroll.gg/d4/tierlists/endgame-tier-list`)
         const tierListSelector = '#main-article';
         const sectionSelector = 'div[class="my-2 flex flex-col gap-4"]';
@@ -24,7 +24,25 @@ describe('Get a list of builds', () => {
         }
 
         await expect(buildList.length).toBeGreaterThan(0);
-        console.log('buildList: ', buildList);
+    })
+
+    it('Get a list of variants for each build', async () => {
+      const variantList = [];
+
+      for (const build of buildList) {
+        await browser.url(build.link)
+        const statPrioritySelector = 'div[class="_headers_1e0ye_7"]';
+
+        const tabs = await $(statPrioritySelector).$$('div');
+        const variants = [];
+        // iterate over each section
+        for (let i = 0; i < tabs.length; i++) {
+          let variant = await $(statPrioritySelector).$$('div')[i].getText();
+          variants.push(variant);
+        }
+        variantList.push({ name: build.name, variants});
+        console.log(`variantList: `, variantList)
+      }
     })
 })
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -131,7 +131,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 120000
     },
 
     //
