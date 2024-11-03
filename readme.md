d3fc49c44d3004296aaebe6c2e8357116f652092
Josh Gould <josh@gameenforcer.com>
2024-11-03 17:57:43 -0500

    generate build list

---
+++ readme.md
@@ -0,0 +1,21 @@
+# d4-uniques-list
+
+This app uses WebDriver.io to gather data from maxroll.gg on what uniques are used in this seasons builds, as well as what their stat priorities are.
+
+## WebDriver.io docs
+https://webdriver.io/docs/gettingstarted.html
+
+## Prerequisites: 
+
+In order to run this app the following must be installed:
+
+- Chrome: v130+
+- Node: v22.11.0+
+
+## Installing this app:
+
+run the following command: `npm ci`
+
+## Running this app:
+
+run the following command: `npm start`
---
+++ test/specs/test.e2e.js
@@ -0,0 +1,30 @@
+const { expect, browser, $ } = require('@wdio/globals');
+
+const buildList = [];
+
+describe('Get a list of builds', () => {
+    it('Go to maxroll tierlist', async () => {
+        await browser.url(`https://maxroll.gg/d4/tierlists/endgame-tier-list`)
+        const tierListSelector = '#main-article';
+        const sectionSelector = 'div[class="my-2 flex flex-col gap-4"]';
+        const buildSelector = 'div[class="border-grey-425 flex items-center border-b pr-4 pb-2 md:border-b-0 md:pb-0 md:pr-0"]';
+
+        const sectionNodes = await $(tierListSelector).$$(sectionSelector);
+
+        // iterate over each section
+        for (let i = 0; i < sectionNodes.length; i++) {
+          let buildNodes = await $(tierListSelector).$$(sectionSelector)[i].$$(buildSelector);
+
+          // iterate over each build
+          for (let j = 0; j < buildNodes.length; j++) {
+            let name = await $(tierListSelector).$$(sectionSelector)[i].$$(buildSelector)[j].$(`a`).getText();
+            let link = await $(tierListSelector).$$(sectionSelector)[i].$$(buildSelector)[j].$(`a`).getAttribute('href');
+            buildList.push({ name, link });
+          }
+        }
+
+        await expect(buildList.length).toBeGreaterThan(0);
+        console.log('buildList: ', buildList);
+    })
+})
+
